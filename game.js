b82f06a0a0c83fe1c77021bf38d8cac948b8eac7
Joaquin Hoyem <113149352+concord01@users.noreply.github.com>
2025-10-01 18:04:41 +0000

    initial commit and importing kaboom

---
+++ game.js
@@ -0,0 +1,12 @@
+// Initializing Kaboom
+kaboom({
+    width:500,
+    height:800,
+    background: [100,200,200]
+});
+
+// This section will be to load assets
+loadSprite("enemy", "https://kaboomjs.com/sprites/gigagantrum.png"))
+
+// Main game scene
+scene()
