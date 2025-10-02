60c3bbded9839d5afeafc26b4026b4259a1f97a2
Joaquin Hoyem <113149352+concord01@users.noreply.github.com>
2025-10-02 18:07:29 +0000

    added player and test enemy

--- game.js
+++ game.js
@@ -9,4 +9,32 @@ kaboom({
 loadSprite("enemy", "https://kaboomjs.com/sprites/gigagantrum.png")
 
 // Main game scene
-scene()
+scene("main",(level) => {
+
+    //player creation
+    const player = add([
+        sprite("enemy"),
+        pos(center().x-70, 400),
+        area({ scale: 0.7 }),
+        body(),
+        timer(),
+        "player",
+    ]);
+    // Player controls
+    onKeyDown("left", () => { player.move(-200, 0); wait(0.25, () => {player.move(200,0)})});
+    onKeyDown("right", () => { player.move(200, 0); wait(0.25, () => {player.move(-200,0)})});
+    onKeyPress("down", () => { player.move(0, 200); wait(0.25, () => {player.move(0,-200)})});
+
+    //Making an enemy
+    const enemy = add([
+    rect(40, 40),
+    pos(center().x, 80),
+    color(255, 0, 0), // Red color
+    area(),
+    anchor("center"),
+    "enemy",
+]);
+})
+
+
+go("main")
