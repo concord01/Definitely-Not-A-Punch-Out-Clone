dd203de08f8ca39a4f622cfcb255da0c1857c605
Joaquin Hoyem <113149352+concord01@users.noreply.github.com>
2025-10-06 18:27:01 +0000

    Added not fully functional attack functions for player and enemy

--- game.js
+++ game.js
@@ -9,19 +9,23 @@ kaboom({
 loadSprite("playerChar", "https://kaboomjs.com/sprites/gigagantrum.png")
 
 // Custom Code
-function enemy_attack(){
-    return {
-        id: "enemy_attack",
-        require: [ "pos", "area" ],
-        add() {
-            rect(5,15),
-            area(),
-            body(),
-            "punch"
-        },
-    };
+function enemy_attack(owner){
+    loop(1, () => {add([
+        rect(5,15),
+        pos(250,50),
+        area(),
+        move(90,1200),
+    ])
+    })
 }
-
+function attack(owner,opponent){
+    add([
+        rect(owner.x,owner.y),
+        color(0,0,0),
+        pos(owner.x,owner.y),
+        move(180,-1200),
+    ])
+}
 // Main game scene
 scene("main",(level) => {
 
@@ -33,11 +37,12 @@ scene("main",(level) => {
         body(),
         timer(),
         "player",
+        hittable = true,
     ]);
     // Player controls
-    onKeyDown("left", () => { player.move(-200, 0); wait(0.25, () => {player.move(200,0)})});
-    onKeyDown("right", () => { player.move(200, 0); wait(0.25, () => {player.move(-200,0)})});
-    onKeyDown("down", () => { player.move(0, 200); wait(0.25, () => {player.move(0,-200)})});
+    onKeyDown("left", () => { player.move(-200, 0); hittable = false; wait(0.25, () => {player.move(200,0)}); hittable = true;});
+    onKeyDown("right", () => { player.move(200, 0); hittable = false; wait(0.25, () => {player.move(-200,0)}); hittable = true;});
+    onKeyDown("down", () => { player.move(0, 200); hittable = false; wait(0.25, () => {player.move(0,-200)}); hittable = true;});
     onKeyPress("z", () => {player.move(0,-300); wait(0.1,() => destroy(enemy),); wait(0.1,() => player.move(0,300))})
 
     //Making an enemy
@@ -47,12 +52,13 @@ scene("main",(level) => {
     color(255, 0, 0), // Red color
     area(),
     anchor("center"),
+    health(5),
+    enemy_attack(player),
     "enemy",
-    //wait(3,() => enemy_attack()),
     ]);
-
+    
     // player.onCollide("punch", (punch, col) =>{
-    //     destroy(player)
+    // destroy(player)
     // })
 })
 
